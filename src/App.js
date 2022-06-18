65218516fcdda3e271ddfcdf68be9c982f417ae7
Joan Montoya <joan15montoya@gmail.com>
2022-06-18 00:24:13 -0500

    feat: integration of style and functions to the calculator

--- src/App.js
+++ src/App.js
@@ -7,93 +7,70 @@ function App() {
   // let counter = 0;
   const [counter, setCounter] = useState(0);
   var [a, setA] = useState(0);
-  var [b, setB] = useState(0);
+  var [val1, setV1] = useState(0);
+  var [val2, setV2] = useState(0);
   const [valores, setVal] = useState({
     valor: "  ",
   });
 
+  function Cambiar(){
+    setV1(val1 = a);
+    setA(a = 0);
+  }
+
   function result(){
+    setV2(val2 = a);
+    setA(a = 0);
     if(valores.valor === "+"){
-    setCounter(parseInt(a) + parseInt(b))
+      setCounter(parseInt(val1) + parseInt(val2))
     }else if(valores.valor === "-"){
-      setCounter(a - b)
+      setCounter(parseInt(val1) - parseInt(val2))
     }else if(valores.valor === "*"){
-      setCounter(a * b)
+      setCounter(parseInt(val1) * parseInt(val2))
     }else if(valores.valor === "/"){
-      setCounter(a / b)
+      setCounter(parseInt(val1) / parseInt(val2))
     }
   }
   
 
   return (
     <div className="App">
-      <h1>{parseInt(a)} {valores.valor} {parseInt(b)}</h1>
-      <h1>Resultado: {counter}</h1>
-      <div className='operadores'>
-        <button className="boton" onClick={() => { setVal((current) => ({ ...current, valor: "+" })); }}>+</button>
-        <button className="boton" onClick={() => { setVal((current) => ({ ...current, valor: "-" })); }}>-</button>
-        <button className="boton" onClick={() => { setVal((current) => ({ ...current, valor: "*" })); }}>*</button>
-        <button className="boton" onClick={() => { setVal((current) => ({ ...current, valor: "/" })); }}>/</button>
-      </div>
-
-      <div className='igual'>
-        <button className="boton1" onClick={() => result()}>=</button>
-      </div>
-
-      <div className='clear'>
-        <button className="boton2"
-        onClick={() => {
-          setA(a = 0);
-          setB(b = 0);
-          setCounter( 0 )
-          setVal((current) => ({ ...current, valor: " " }));
-          }}>Limpiar</button>
-      </div>
-      
-      <br></br>
-
-      <div className='container'>
-        <div className="operando1">
-          <h2>Operando 1</h2>
+      <div className='resultados'>
+          <h2>{parseInt(val1)} {valores.valor} {parseInt(val2)} = {counter}</h2>
+          <h1>{parseInt(a)}</h1>
+        </div>
+          <button className="boton1"
+            onClick={() => {
+            setA(a = 0);
+            setCounter( 0 );
+            setV1(0);
+            setV2(0);
+            setVal((current) => ({ ...current, valor: " " }));
+            }}>Clear
+          </button>
+          <br></br>
+          <br></br>
           <button className="boton" onClick={() => {setA(a = a + "1");}}>1</button>
           <button className="boton" onClick={() => {setA(a = a + "2");}}>2</button>
           <button className="boton" onClick={() => {setA(a = a + "3");}}>3</button>
+          <button className="boton" onClick={() => { setVal((current) => ({ ...current, valor: "+" })); Cambiar()}}>+</button>
           <br></br>
           <button className="boton" onClick={() => {setA(a = a + "4");}}>4</button>
           <button className="boton" onClick={() => {setA(a = a + "5");}}>5</button>
           <button className="boton" onClick={() => {setA(a = a + "6");}}>6</button>
+          <button className="boton" onClick={() => { setVal((current) => ({ ...current, valor: "-" })); Cambiar()}}>-</button>
           <br></br>
           <button className="boton" onClick={() => {setA(a = a + "7");}}>7</button>
           <button className="boton" onClick={() => {setA(a = a + "8");}}>8</button>
           <button className="boton" onClick={() => {setA(a = a + "9");}}>9</button>
+          <button className="boton" onClick={() => { setVal((current) => ({ ...current, valor: "*" })); Cambiar()}}>*</button>
           <br></br>
           <button className="boton" >0</button>
           <button className="boton" onClick={() => {setA(a = a + "0");}}>0</button>
           <button className="boton" >0</button>
+          <button className="boton" onClick={() => { setVal((current) => ({ ...current, valor: "/" })); Cambiar()}}>/</button>
           <br></br>
-        </div>
-        <div className="operando2">
-        <h2>Operando 2</h2>
-          <button className="boton" onClick={() => {setB(b = b + "1");}}>1</button>
-          <button className="boton" onClick={() => {setB(b = b + "2");}}>2</button>
-          <button className="boton" onClick={() => {setB(b = b + "3");}}>3</button>
-          <br></br>
-          <button className="boton" onClick={() => {setB(b = b + "4");}}>4</button>
-          <button className="boton" onClick={() => {setB(b = b + "5");}}>5</button>
-          <button className="boton" onClick={() => {setB(b = b + "6");}}>6</button>
-          <br></br>
-          <button className="boton" onClick={() => {setB(b = b + "7");}}>7</button>
-          <button className="boton" onClick={() => {setB(b = b + "8");}}>8</button>
-          <button className="boton" onClick={() => {setB(b = b + "9");}}>9</button>
-          <br></br>
-          <button className="boton" >0</button>
-          <button className="boton" onClick={() => {setB(b = b + "0");}}>0</button>
-          <button className="boton" >0</button>
-          <br></br>
-        </div>
-      </div>
-      
-
+          <button className="boton1" onClick={() => result()}>=</button>
     </div>
   );
 
